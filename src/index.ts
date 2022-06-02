49c9de68077136c616e2cb0047323e9e396ce15e
melkerydman <melkerrydman@gmail.com>
2022-06-02 21:00:34 +0200

    Basic light, ground and camera controls

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 import * as THREE from "three";
-
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import Stats from "three/examples/jsm/libs/stats.module";
 
 let camera: THREE.PerspectiveCamera,
@@ -25,14 +25,40 @@ function init() {
   camera.position.set(0, 0, 250);
 
   // SCENE
-
   scene = new THREE.Scene();
+  scene.add(new THREE.AxesHelper(50));
 
   // RENDERER
-
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  // LIGHT
+  const light = new THREE.AmbientLight();
+  scene.add(light);
+
+  // GROUND
+  const groundGeometry = new THREE.PlaneGeometry(10000, 10000);
+  const groundMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+  groundMaterial.color.setHSL(0.095, 1, 0.75);
+
+  const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+  // ground.position.y = -33;
+  ground.rotation.x = -Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add(ground);
+
+  //CONTROLS
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
